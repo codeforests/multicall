87406f5f363d6d09081c22948d3c52d7570ec821
codeforests <likunsolutions@gmail.com>
2021-12-04 23:18:46 +0800

    remove hard coded contract address

--- scripts/multicall.py
+++ scripts/multicall.py
@@ -1,15 +1,17 @@
 from brownie import Contract, interface, multicall
 
+ETH_USD_FEED_ADDRESS = "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419"
+UNISWAP_MULTICALL2 = "0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696"
 
 def main():
     price_feed = Contract.from_abi(
         "feed", 
-        "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
+        ETH_USD_FEED_ADDRESS,
         interface.AggregatorV3Interface.abi)
 
     rounds = []
     latest_round = price_feed.latestRoundData()[0]
-    multicall(address="0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696")
+    multicall(address=UNISWAP_MULTICALL2)
     with multicall:
         for round_id in range(latest_round, latest_round - 50, -1):
             round_data = price_feed.getRoundData(round_id)
